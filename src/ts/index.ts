6722c704b9f665e3a6609f2f2ef7b5d02a5c9ff0
Alana <mir.alana2294@outlook.com>
2026-06-24 17:49:16 -0500

    added button and moved CSS

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -5,13 +5,20 @@
 const app = document.getElementById('app');
 
 if (app) {
-    app.innerHTML = `
+    app.innerHTML += `
         <h1>Welcome to the Learning Center Mummy</h1>
         <p>A modern TypeScript website by me, TIFFANY!</p>
         <p>
-            Let's Get started bruh
+            Let's get started bruh
         </p>
     `;
+    
+    const button = document.createElement('button');
+    button.textContent = 'Let\'s Go!';
+    app.appendChild(button);
+    button.addEventListener('click', () => {
+        location.href = 'home.html';
+    });
 }
 
 console.log('Learning Center loaded successfully!');
