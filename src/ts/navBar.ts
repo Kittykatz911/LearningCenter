b3fc6982e6d2caab4875a52c9c114af22139fdce
Alana Henry <mir.alana2294@gmail.com>
2026-07-16 15:42:38 -0500

    Added logic to dropdown menus

--- src/ts/navBar.ts
+++ src/ts/navBar.ts
@@ -7,9 +7,11 @@ menuButton?.addEventListener("click", () => {
     const menuArrow = document.getElementById("menuIcon")!;
     const pagesWrapper = document.getElementById("pages-wrapper")!;
     const links = pagesWrapper.querySelectorAll("a");
+    const pageContent = document.querySelector<HTMLElement>(".content")!;
 
     if (navBar.style.width === "170px") {
         navBar.style.width = defaultWidth;
+        pageContent.style.paddingLeft = "70px";
         menuArrow.textContent = "menu";
         menuArrow.style.paddingLeft = "";
 
@@ -17,23 +19,74 @@ menuButton?.addEventListener("click", () => {
         pagesWrapper.style.height = "";
         links.forEach((link) => {
             link.style.display = "none";
+            link.style.backgroundColor = "";
             link.style.opacity = "0";
         });
         pagesWrapper.querySelector("hr")!.style.display = "none";
     }
     else {
         navBar.style.width = "170px";
+        pageContent.style.paddingLeft = "190px";
         menuArrow.textContent = "arrow_back_ios";
         menuArrow.style.paddingLeft = "10px";
 
         pagesWrapper.style.width = "170px";
-        pagesWrapper.style.height = "280px";
+        pagesWrapper.style.height = "350px";
         links.forEach((link) => {
             link.style.display = "block";
             link.style.opacity = "1";
         });
         pagesWrapper.querySelector("hr")!.style.display = "block";
     }
+});
+const categories = document.querySelectorAll<HTMLElement>(".page-links");
+
+categories?.forEach((category) => {
+    category.addEventListener("click", (mouseEvent) => {
+        const target = mouseEvent.target as HTMLElement;
+        if (target.classList.contains("drop")) {
+
+            categories?.forEach((cat) => {
+                if (cat !== target.parentElement) {
+                    cat.style.backgroundColor = "";
+                }
+            });
+            // then make the dropdown content visible
+            mouseEvent.preventDefault();
+            if (target.parentElement?.classList.contains("services")) {
+                const serviceOptions = document.getElementById("service-options");
+                if (serviceOptions!.style.display === "block") {
+                    serviceOptions!.style.display = "none";
+
+                    target.parentElement!.style.backgroundColor = "";
+                    serviceOptions!.style.backgroundColor = "";
+                }
+                else {
+                    serviceOptions!.style.display = "block";
+                    serviceOptions!.style.backgroundColor = "white";
+
+                    target.parentElement!.style.backgroundColor = "white";
+                }
+
+            }
+            else if (target.parentElement?.classList.contains("aaps")) {
+                target.parentElement!.style.backgroundColor = "white";
+            }
+            else if (target.parentElement?.classList.contains("bookings")) {
+                target.parentElement!.style.backgroundColor = "white";
+            }
+            else if (target.parentElement?.classList.contains("lessonry")) {
+                target.parentElement!.style.backgroundColor = "white";
+            }
+            else if (target.parentElement?.classList.contains("mentors")) {
+                target.parentElement!.style.backgroundColor = "white";
+            }
+            else if (target.parentElement?.classList.contains("professional-development")) {
+                target.parentElement!.style.backgroundColor = "white";
+            }
+
+        }
+    });
 });
 
 nightModeButton?.addEventListener("click", () => {
