b71b608a3e307fc10a5110a8fb6c610504d8a64d
Alana Henry <mir.alana2294@gmail.com>
2026-07-15 20:22:01 -0500

    Added transitions and ts for new links

--- src/ts/navBar.ts
+++ src/ts/navBar.ts
@@ -5,15 +5,34 @@ const nightModeButton = document.getElementById("darkmode");
 
 menuButton?.addEventListener("click", () => {
     const menuArrow = document.getElementById("menuIcon")!;
+    const pagesWrapper = document.getElementById("pages-wrapper")!;
+    const links = pagesWrapper.querySelectorAll("a");
+
     if (navBar.style.width === "170px") {
         navBar.style.width = defaultWidth;
         menuArrow.textContent = "arrow_forward_ios";
         menuArrow.style.paddingLeft = "";
+
+        pagesWrapper.style.width = "";
+        pagesWrapper.style.height = "";
+        links.forEach((link) => {
+            link.style.display = "none";
+            link.style.opacity = "0";
+        });
+        pagesWrapper.querySelector("hr")!.style.display = "none";
     }
     else {
         navBar.style.width = "170px";
         menuArrow.textContent = "arrow_back_ios";
         menuArrow.style.paddingLeft = "10px";
+
+        pagesWrapper.style.width = "170px";
+        pagesWrapper.style.height = "280px";
+        links.forEach((link) => {
+            link.style.display = "block";
+            link.style.opacity = "1";
+        });
+        pagesWrapper.querySelector("hr")!.style.display = "block";
     }
 });
 
